6a96d4f74cd506bc6768181192acae498113563b
nokia0315 <naoki11923611@gmail.com>
2020-11-24 19:14:17 +0000

    usersの画面の編集

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '5337789d10ec0d9ec047edecf09fa522d9f9ee16b48b6f87fbfc5273500966807def742497cedaa7254a5ecf533017bbb54bf34e2b77102ca75b2ab659e940e8'
