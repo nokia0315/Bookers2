50a32468e5b29b1903271ef76995d1cd8f2ea3a9
nokia0315 <naoki11923611@gmail.com>
2020-11-26 19:28:36 +0000

    [Finish]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to books_path
+    redirect_to book_path(@book.id)
     else
     render :index
     end
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = current_user
+
     @books = Book.new
   end
 
