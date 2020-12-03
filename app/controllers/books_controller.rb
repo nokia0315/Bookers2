ff37295d62c77f33b9b7887d07be63e052a1c2c2
nokia0315 <naoki11923611@gmail.com>
2020-12-03 13:02:45 +0000

    [Finish]応用課題③

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,7 +43,8 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @books = Book.new
-
+    @book_comment = BookComment.new
+    @user = current_user
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
+  resources :books do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
+ end
   resources :users, only: [:show, :edit, :update, :index]
   post 'users/:id' => 'books#create'
 
